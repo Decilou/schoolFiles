ef989c42c8804b1d544195e2764735f868895a14
”gabrielbrattgard” <gabriel.brattgard@gmail.com>
2019-11-15 13:15:48 +0100

    Created objects. Added .toString-override.

--- src/Volvo240.java
+++ src/Volvo240.java
@@ -1,17 +1,20 @@
 import java.awt.*;
 
 /**
- * Class for
+ * Class for Volvo 240.
  */
 public class Volvo240 extends Car {
 
     public final static double trimFactor = 1.25;
 
-
     public Volvo240(int nrDoors, double enginePower, double currentSpeed, Color color, String modelName) {
         super(nrDoors, enginePower, currentSpeed, color, modelName);
     }
 
+    /**
+     * Calculates and returns the speed factor.
+     * @return speed factor of the car
+     */
     public double speedFactor() {
         return getEnginePower() * 0.01 * trimFactor;
     }
