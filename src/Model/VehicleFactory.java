ebaef8467e35ac3f41324e8050d84102f30de91a
gabrielbrattgard <56637962+gabrielbrattgard@users.noreply.github.com>
2019-12-20 14:22:44 +0100

    Fixed y-issue in vehicleFactory.

--- src/Model/VehicleFactory.java
+++ src/Model/VehicleFactory.java
@@ -1,27 +1,35 @@
 package Model;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 class VehicleFactory {
 
-    //TODO: Replace this y-counter with list.size()*100
-    int y = 0;
+    private ArrayList<Vehicle> vehicles;
+    private int y;
+
+    VehicleFactory(ArrayList<Vehicle> vehicles) {
+        this.vehicles = vehicles;
+    }
+
     Vehicle createVehicle(VehicleModelName name) {
 
         if (name == null) {
             return null;
         } else if (name.equals(VehicleModelName.SAAB)) {
-            y = y + 100;
+            y = vehicles.size() * 80;
             return new Saab95(Color.black, 10, y);
 
         } else if (name.equals(VehicleModelName.VOLVO)) {
-            y = y + 100;
+            y = vehicles.size() * 80;
             return new Volvo240(Color.gray, 10, y);
 
         } else if (name.equals(VehicleModelName.SCANIA)) {
-            y = y + 100;
+            y = vehicles.size() * 80;
             return new Scania(Color.red, 10, y);
         }
         return null;
     }
+
+
 }
